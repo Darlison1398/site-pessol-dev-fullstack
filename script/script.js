49f81b049311316907d68fc1d8d2220f3aaf43c1
Darlison1398 <drsilva123jd@gmail.com>
2024-05-22 17:22:44 -0300

    Atualizado 22-05

---
+++ script/script.js
@@ -0,0 +1,12 @@
+
+function zap() {
+    window.open('https://api.whatsapp.com/send?phone=5511999999999&text=Olá, tudo bem?');
+}
+
+function din() {
+    window.open('https://www.linkedin.com/in/darlison-silva-86113a191');
+}
+
+function hub() {
+    window.open('https://github.com/Darlison1398');
+}
